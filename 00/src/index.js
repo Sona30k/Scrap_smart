dd6b0fd4642a8ac066766b3ba1964061827ac501
Sona30k <sona22kerketta@gmail.com>
2024-12-13 11:30:55 +0530

    added

---
+++ 00/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Hey from './1';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <Hey />
+  </React.StrictMode>
+);
+
+
